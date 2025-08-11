64e51004ea17dcc7cca58e6b7153b2a6542c52d9
deepakparam <deepakparam@gmail.com>
2025-08-11 12:51:35 +0530

    task update

---
+++ task1.py
@@ -0,0 +1,15 @@
+num1 = int(input('Enter the first number:'))
+
+num2 = int(input('Enter the second number:'))
+
+addnum = num1 + num2
+
+subnum = num1 - num2
+
+mulnum = num1 * num2
+
+divnum = num1 / num2
+print('\nAddition : ', addnum)
+print('Subtraction : ', subnum)
+print('Multiplication : ', mulnum)
+print('Division : ', divnum)
---
+++ task2.py
@@ -0,0 +1,6 @@
+fname = input('Enter first name : ')
+lname = input('Enter last name : ')
+
+fullname = fname + ' ' + lname + '!'
+
+print("\nHello,", fullname, "Welcome to the Python program.")
